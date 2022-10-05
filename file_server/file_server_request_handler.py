c874366438d05226d61d8b10d848f045ef06902e
Ziv Dayan <ziv987@gmail.com>
2022-10-05 15:03:17 +0300

    added base of request handler

--- file_server/file_server_request_handler.py
+++ file_server/file_server_request_handler.py
@@ -1,4 +1,37 @@
+from structures.request import FileServerRequest
+from typing import Callable, Dict
+
+
 class FileServerRequestHandler:
-    def __init__(self):
+
+    def __init__(self, raw_request):
+        self.raw_request = raw_request
+
+    def handle(self):
+        request: FileServerRequest = FileServerRequest.parse_binary_request(self.raw_request)
+        requested_function = self.request_to_function_mapping.get(request.code)
+
+        requested_function()
+
+    @property
+    def request_to_function_mapping(self) -> Dict[Callable]:
+        return {1100: self.register_user, 1101: self.send_public_key, 1103: self.send_file, 1104: self.valid_crc,
+                1105: self.invalid_crc, 1106: self.invalid_crc_terminating}
+
+    def register_user(self):
         pass
-    def registry_
+
+    def send_public_key(self):
+        pass
+
+    def send_file(self):
+        pass
+
+    def valid_crc(self):
+        pass
+
+    def invalid_crc(self):
+        pass
+
+    def invalid_crc_terminating(self):
+        pass
---
+++ structures/request.py
@@ -0,0 +1,51 @@
+import struct
+
+
+class FileServerRequest:
+    def __init__(self, client_id, version, code, payload_size, payload):
+        self.client_id = client_id
+        self.version = version
+        self.code = code
+        self.payload_size = payload_size
+        self.payload = payload
+
+    @classmethod
+    def parse_binary_request(cls, raw_request):
+        '''
+             +----+-----+-------+------+----------+----------+
+              |client_id | version |  code  | payload_size | payload |
+              +----+-----+-------+------+----------+----------+
+              |    16    |     1   |    2   |      4       | Variable |
+              +----+-----+-------+------+----------+----------+
+
+       :return:
+
+
+       '''
+
+        try:
+
+            client_id = None
+            version = None
+            code = None
+            payload = None
+
+            format = '<'  # Little endian annotation
+            format += '16s'  # client_id
+            format += 'c'  # version
+            format += 'B'  # code
+            format += 'I'  # unsigned payload size
+            payload_size = int(raw_request[20:24])  # extract payload size from raw request
+            format += f"{payload_size}s"
+
+            struct.unpack(format, client_id, version, code, payload_size, payload)
+
+            return cls(client_id=client_id, version=version, code=code, payload_size=payload_size, payload=payload)
+
+        except Exception:
+            raise InvalidException
+
+
+class InvalidException(Exception):
+    """Base class for other exceptions"""
+    pass
